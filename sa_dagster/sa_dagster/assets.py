2d2833df49697e4062fafdd732b8f1cb6aec34bd
Stephan Ohm <github@stephan-ohm.com>
2024-03-04 22:02:25 +0100

    Scan network and connect to starke db

--- sa_dagster/sa_dagster/assets.py
+++ sa_dagster/sa_dagster/assets.py
@@ -1,17 +1,23 @@
 from dagster import asset
 
 import socket
-
 import sqlalchemy
-from sqlalchemy import create_engine
+import os
+import configparser
 
-STARKE_PRAXIS_PORT = 1433
-STARKE_PRAXIS_USER = 'sa'
-STARKE_PRAXIS_PASSWORD = STARKE_PRAXIS_PASSWORD
+from dotenv import load_dotenv
+load_dotenv()
 
-available_possible_hosts = []
-starke_praxis_host = None
-starke_praxis_db = None
+STARKE_PRAXIS_PORT = int(os.getenv('STARKE_PRAXIS_PORT'))
+STARKE_PRAXIS_USER = os.getenv('STARKE_PRAXIS_USER')
+STARKE_PRAXIS_PASSWORD = os.getenv('STARKE_PRAXIS_PASSWORD')
+
+CONFIG_FILE_PATH = os.getenv('CONFIG_FILE_PATH')
+
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE_PATH)
+
+
 
 @asset
 def get_local_ip():
@@ -27,35 +33,62 @@ def get_local_ip():
     finally:
         s.close()
 
-    with open('../data/temp/ip.txt', 'w') as f:
-        f.write(local_ip)
+    config.set('NETWORK', 'local_ip', local_ip)
+
+    with open(CONFIG_FILE_PATH, 'w') as configfile:
+        config.write(configfile)
 
 
 @asset(deps=[get_local_ip])
 def get_list_of_host_with_open_mssql_port():
     # https://www.tutorialspoint.com/python_penetration_testing/python_penetration_testing_network_scanner.htm
 
-    with open('../data/temp/ip.txt', 'r') as f:
-        local_ip = f.read()
+    local_ip = config.get('NETWORK', 'local_ip')
 
     ip_range = '.'.join(local_ip.split('.')[:3])
 
     for i in range(1, 255):
-        ip = f'{ip_range}.{i}'
+        ip: str = f'{ip_range}.{i}'
         socket.setdefaulttimeout(0.1)
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = s.connect_ex((ip, STARKE_PRAXIS_PORT))
+        # 0: open, 1: closed
         if result == 0:
-            available_possible_hosts.append(ip)
+            break
+
+    config.set('NETWORK', 'last_known_starke_mssql_server', ip)
+
+    with open(CONFIG_FILE_PATH, 'w') as configfile:
+        config.write(configfile)
 
 
 @asset(deps=[get_list_of_host_with_open_mssql_port])
-def check_starke_praxis_host():
-    for ip in available_possible_hosts:
-        try:
-            #conn = pymssql.connect(server=ip, user=STARKE_PRAXIS_USER, password=STARKE_PRAXIS_PASSWORD)
-            #conn.close()
-            return ip
-        except:
-            pass
+def get_correct_db():
+    starke_mssql_server = config.get('NETWORK', 'last_known_starke_mssql_server')
+
+    engine = sqlalchemy.create_engine(
+        'mssql+pyodbc://' + STARKE_PRAXIS_USER + ':' + STARKE_PRAXIS_PASSWORD + '@' + starke_mssql_server + '/master?driver=ODBC+Driver+18+for+SQL+Server',
+        connect_args={"TrustServerCertificate": "yes"})
+
+    query = """
+                SELECT 
+                        sysdatabases.name
+                FROM
+                        master.sys.sysdatabases
+                WHERE   1=1
+                  AND   sysdatabases.name LIKE 'SP9_%'
+                  ;      
+            """
+
+    con = engine.connect()
+    
+    result = con.execute(sqlalchemy.text(query))
+
+    config.set('STARKE_PRAXIS', 'database', result.fetchall()[0][0])
+
+    with open(CONFIG_FILE_PATH, 'w') as configfile:
+        config.write(configfile)
+
+
+
