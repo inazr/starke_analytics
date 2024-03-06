49b2c6e9036bbadf5c41d5540d054737a669f291
Stephan Ohm <github@stephan-ohm.com>
2024-03-06 21:59:42 +0100

    created extract & load for rezept, termin, mitarbeiter

--- sa_dagster/sa_dagster/__init__.py
+++ sa_dagster/sa_dagster/__init__.py
@@ -1,9 +1,19 @@
 from dagster import Definitions, load_assets_from_modules
-
+from dagster_duckdb import DuckDBResource
+import os
 from . import assets
+from dotenv import load_dotenv
+load_dotenv()
+
+DUCKDB_PATH = os.getenv('DUCKDB_PATH')
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
     assets=all_assets,
+    resources={
+        "duckdb": DuckDBResource(
+            database=DUCKDB_PATH + '/starke_praxis.duckdb',
+        )
+    },
 )
--- sa_dagster/sa_dagster/assets.py
+++ sa_dagster/sa_dagster/assets.py
@@ -5,6 +5,9 @@ import sqlalchemy
 import os
 import configparser
 
+import pandas as pd
+from dagster_duckdb import DuckDBResource
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -91,4 +94,262 @@ def get_correct_db():
         config.write(configfile)
 
 
+@asset(deps=[get_correct_db])
+def create_starke_schema(duckdb: DuckDBResource) -> None:
+    create_schema_query = """
+                            CREATE SCHEMA IF NOT EXISTS starke
+                            ;
+                          """
 
+    with duckdb.get_connection() as conn:
+        conn.execute(create_schema_query)
+
+
+@asset(deps=[create_starke_schema])
+def extract_load_termin(duckdb: DuckDBResource) -> None:
+    starke_mssql_server = config.get('NETWORK', 'last_known_starke_mssql_server')
+    starke_praxis_db = config.get('STARKE_PRAXIS', 'database')
+
+    engine = sqlalchemy.create_engine(
+        'mssql+pyodbc://' + STARKE_PRAXIS_USER + ':' + STARKE_PRAXIS_PASSWORD + '@' + starke_mssql_server + '/' + starke_praxis_db + '?driver=ODBC+Driver+18+for+SQL+Server',
+        connect_args={"TrustServerCertificate": "yes"})
+
+    con = engine.connect()
+
+    query = """
+                SELECT 
+                        TERMIN.Nr,
+                        TERMIN.REZ_Nr,
+                        TERMIN.Datum,
+                        TERMIN.MIT_Kurzname,
+                        TERMIN.Brutto,
+                        TERMIN.Zuzahlung,
+                        TERMIN.Kennzeichen,
+                        TERMIN.Ausgefallen,
+                        TERMIN.Multi,
+                        TERMIN.Begruendung,
+                        HASHBYTES('SHA2_256', TERMIN.Text) AS Text, -- possible personal data in text
+                        TERMIN.ChangeDate,
+                        TERMIN.ChangeTime,
+                        TERMIN.BereitsErhalten,
+                        TERMIN.Teletherapie,
+                        TERMIN.AzhUnterbrechung,
+                        TERMIN.DcStatus
+                FROM
+                        dbo.TERMIN
+                WHERE   1=1
+                ;      
+            """
+
+
+    result = con.execute(sqlalchemy.text(query))
+
+    df_result = pd.DataFrame(result.fetchall())
+
+    with duckdb.get_connection() as conn:
+        conn.execute("DROP TABLE IF EXISTS starke.raw_termine;")
+        conn.execute("CREATE TABLE IF NOT EXISTS starke.raw_termine AS SELECT * FROM df_result;")
+
+
+@asset(deps=[create_starke_schema])
+def extract_load_mitarbeiter(duckdb: DuckDBResource) -> None:
+    starke_mssql_server = config.get('NETWORK', 'last_known_starke_mssql_server')
+    starke_praxis_db = config.get('STARKE_PRAXIS', 'database')
+
+    engine = sqlalchemy.create_engine(
+        'mssql+pyodbc://' + STARKE_PRAXIS_USER + ':' + STARKE_PRAXIS_PASSWORD + '@' + starke_mssql_server + '/' + starke_praxis_db + '?driver=ODBC+Driver+18+for+SQL+Server',
+        connect_args={"TrustServerCertificate": "yes"})
+
+    con = engine.connect()
+
+    query = """
+                SELECT 
+                        MITARBEITER.Nr,
+                        MITARBEITER.Kurzname,
+                        MITARBEITER.Name,
+                        MITARBEITER.Nachname,
+                        MITARBEITER.Vorname,
+                        MITARBEITER.Geschlecht,
+                        MITARBEITER.Titel,
+                        MITARBEITER.Namenszusatz,
+                        MITARBEITER.Geburtsdatum,
+                        MITARBEITER.IK,
+                        MITARBEITER.Abrechnungscode,
+                        MITARBEITER.Rechte,
+                        MITARBEITER.Passwort,
+                        MITARBEITER.Praxistermine,
+                        MITARBEITER.Hausbesuche,
+                        MITARBEITER.Wegegelder,
+                        MITARBEITER.Aktiv,
+                        MITARBEITER.Passiv,
+                        MITARBEITER.Privat,
+                        MITARBEITER.Porto,
+                        MITARBEITER.Ausgeschieden,
+                        MITARBEITER.Kontonummer,
+                        MITARBEITER.Kreditinstitut,
+                        MITARBEITER.BLZ,
+                        MITARBEITER.Notiz,
+                        MITARBEITER.ADR_Nr,
+                        MITARBEITER.ChangeDate,
+                        MITARBEITER.ChangeTime,
+                        MITARBEITER.Sollzeit,
+                        MITARBEITER.Qualifikation,
+                        MITARBEITER.Arbeitszeit,
+                        MITARBEITER.Ort,
+                        MITARBEITER.Berufsurkunde
+                FROM
+                        dbo.MITARBEITER
+                WHERE   1=1
+                ;      
+            """
+
+    result = con.execute(sqlalchemy.text(query))
+
+    df_result = pd.DataFrame(result.fetchall())
+
+    with duckdb.get_connection() as conn:
+        conn.execute("DROP TABLE IF EXISTS starke.raw_mitarbeiter;")
+        conn.execute("CREATE TABLE IF NOT EXISTS starke.raw_mitarbeiter AS SELECT * FROM df_result;")
+
+
+@asset(deps=[create_starke_schema])
+def extract_load_rezept(duckdb: DuckDBResource) -> None:
+    starke_mssql_server = config.get('NETWORK', 'last_known_starke_mssql_server')
+    starke_praxis_db = config.get('STARKE_PRAXIS', 'database')
+
+    engine = sqlalchemy.create_engine(
+        'mssql+pyodbc://' + STARKE_PRAXIS_USER + ':' + STARKE_PRAXIS_PASSWORD + '@' + starke_mssql_server + '/' + starke_praxis_db + '?driver=ODBC+Driver+18+for+SQL+Server',
+        connect_args={"TrustServerCertificate": "yes"})
+
+    con = engine.connect()
+
+    query = """
+                SELECT
+                        REZEPT.Nr,
+                        REZEPT.Euro,
+                        REZEPT.Art,
+                        REZEPT.Datum,
+                        REZEPT.Soll,
+                        REZEPT.FrequenzVon,
+                        REZEPT.FrequenzBis,
+                        REZEPT.FrequenzOK,
+                        REZEPT.Ist,
+                        REZEPT.Termine,
+                        REZEPT.Ausgefallen,
+                        REZEPT.Verordnung,
+                        REZEPT.Behandlungsbeginn,
+                        REZEPT.Hausbesuch,
+                        REZEPT.Heim,
+                        REZEPT.Bericht,
+                        REZEPT.Gruppentherapie,
+                        REZEPT.Kilometer,
+                        REZEPT.Brutto,
+                        REZEPT.Zuzahlung,
+                        REZEPT.Pauschale,
+                        REZEPT.Netto,
+                        REZEPT.MwSt,
+                        REZEPT.Ausfall,
+                        REZEPT.IK,
+                        REZEPT.Abrechnungscode,
+                        REZEPT.Zustand,
+                        REZEPT.Voranschlag,
+                        REZEPT.ZUZ_Status,
+                        REZEPT.ZUZ_Datum,
+                        REZEPT.ZUZ_MIT_Kurzname,
+                        REZEPT.ZUZ_Betrag,
+                        REZEPT.ZUZ_Mahnung,
+                        REZEPT.Eigenanteil,
+                        REZEPT.Arbeitsunfall,
+                        REZEPT.Unfalltag,
+                        REZEPT.Unfallbetrieb,
+                        REZEPT.BVG,
+                        REZEPT.Genehmigungskennzeichen,
+                        REZEPT.Genehmigungsdatum,
+                        REZEPT.Ausland,
+                        REZEPT.EWRCH,
+                        HASHBYTES('SHA2_256', REZEPT.PAT_Name) AS PAT_Name, -- possible personal data in text
+                        HASHBYTES('SHA2_256', CAST(REZEPT.PAT_Geburtsdatum AS VARCHAR(6))) AS PAT_Geburtsdatum, -- possible personal data in text
+                        HASHBYTES('SHA2_256', REZEPT.PAT_Versichertennummer) AS PAT_Versichertennummer, -- possible personal data in text
+                        REZEPT.PAT_VKGueltigkeit,
+                        REZEPT.PAT_VKZ,
+                        REZEPT.PAT_Status,
+                        REZEPT.PAT_Frei,
+                        REZEPT.PAT_FreiVon,
+                        REZEPT.PAT_FreiBis,
+                        REZEPT.DGZ_Nummer,
+                        REZEPT.DGZ_Wechsel,
+                        REZEPT.DIA_Nummer,
+                        REZEPT.DIA_Diagnose,
+                        REZEPT.HMROK,
+                        REZEPT.MIT_Kurzname,
+                        REZEPT.PAT_ADR_Nr,
+                        REZEPT.PAT_EMP_Nr,
+                        REZEPT.KAS_IK,
+                        REZEPT.KAS_Name,
+                        REZEPT.TAR_Name,
+                        REZEPT.TAR_Gueltigkeit,
+                        REZEPT.LTA_Nummer_0,
+                        REZEPT.LTA_Nummer_1,
+                        REZEPT.LTA_Nummer_2,
+                        REZEPT.LTA_Faktor_0,
+                        REZEPT.LTA_Faktor_1,
+                        REZEPT.LTA_Faktor_2,
+                        REZEPT.LTA_Weitere,
+                        REZEPT.BET_Nummer,
+                        REZEPT.ARZ_LANR,
+                        REZEPT.ARZ_Nummer,
+                        HASHBYTES('SHA2_256', REZEPT.ARZ_Name) AS ARZ_Name, -- possible personal data in text
+                        REZEPT.Zahlungsart,
+                        REZEPT.Erster,
+                        REZEPT.Letzter,
+                        REZEPT.ChangeDate,
+                        REZEPT.ChangeTime,
+                        REZEPT.Text,
+                        REZEPT.BeginnOK,
+                        REZEPT.FristOK,
+                        REZEPT.AROK,
+                        REZEPT.ICDOK,
+                        REZEPT.REC_Nummer,
+                        REZEPT.Ungeprueft,
+                        REZEPT.DocGuid,
+                        REZEPT.DocArchiv,
+                        REZEPT.Entlass,
+                        REZEPT.Anonym,
+                        REZEPT.ZUZ_Aufforderung,
+                        REZEPT.KorrekturNr,
+                        REZEPT.Verarbeitungskennzeichen,
+                        REZEPT.Formular,
+                        REZEPT.HDG_Nummer,
+                        REZEPT.LS,
+                        REZEPT.Diagnosetext,
+                        REZEPT.Dringlich,
+                        REZEPT.Heilmittelbereich,
+                        REZEPT.ScannedHeilmittel,
+                        REZEPT.FrequenzEinheit,
+                        REZEPT.ZUZ_Muendlich,
+                        REZEPT.ZUZ_Exkasso,
+                        REZEPT.ZUZ_DatumExkasso,
+                        REZEPT.Ablaufdatum,
+                        REZEPT.LhbBvb,
+                        REZEPT.AzhVersion,
+                        REZEPT.DcStatus,
+                        REZEPT.DcRequested,
+                        REZEPT.DcConfirmed,
+                        REZEPT.DcDeclined,
+                        REZEPT.UnterschriftArzt,
+                        REZEPT.Aenderungen,
+                        REZEPT.Langzeit,
+                        REZEPT.Kostenzusage,
+                        REZEPT.KostenzusageDatum
+                FROM
+                        dbo.REZEPT
+                WHERE   1=1
+                ;      
+            """
+
+    result = con.execute(sqlalchemy.text(query))
+    df_result = pd.DataFrame(result.fetchall())
+
+    with duckdb.get_connection() as conn:
+        conn.execute("DROP TABLE IF EXISTS starke.raw_rezept;")
+        conn.execute("CREATE TABLE IF NOT EXISTS starke.raw_rezept AS SELECT * FROM df_result;")
