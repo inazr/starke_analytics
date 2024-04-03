3626bd07d75f8c288fd7e28905da70c8bc3d7ae0
Stephan Ohm <github@stephan-ohm.com>
2024-04-03 11:22:40 +0200

    added executable file

--- dagster_dev_startup.sh
+++ dagster_dev_startup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# This file should be executable.
+# Link: https://ionic.zendesk.com/hc/en-us/articles/360000160067-Adding-execute-permissions-using-git
 source /home/ubuntu/starke_analytics/venv/bin/activate
 cd /home/ubuntu/starke_analytics/sa_dagster
 dagster dev -h 0.0.0.0 -p 3000
